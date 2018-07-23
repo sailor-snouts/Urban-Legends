9249733e6d5abcd75d5275cb5e2b47bfbffa263c
Robert Parker <rparker@kodama.design>
2018-07-22 17:49:27 -0700

    distance and timer scripts

---
+++ Assets/Scripts/Distance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance : MonoBehaviour {
+    private Vector2 start;
+    [SerializeField]
+    private float distance;
+
+    public void Start()
+    {
+        this.start = this.transform.position;
+    }
+
+    public void Update()
+    {
+        this.distance = Vector2.Distance(this.transform.position, this.start);
+    }
+
+    public bool hasTraveled(float distance)
+    {
+        return this.distance > distance;
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    [SerializeField]
+    private float timer;
+
+    public bool hasTime()
+    {
+        return this.timer > 0;
+    }
+
+    public void Update()
+    {
+        this.timer -= Time.deltaTime;
+    }
+}
