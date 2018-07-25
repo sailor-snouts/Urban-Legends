5f8826dfaee4914e67d9855c938315adb5d5468e
Joshua Skinner <jkyuusai@gmail.com>
2018-07-24 21:51:59 -0700

    Add bird and slopeMovement script

---
+++ Assets/Scripts/SlopeMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlopeMovement : MonoBehaviour {
+    [SerializeField, Range(0, 15)]
+    private float speed = 7.5f;
+    [SerializeField, Range(-2, 2)]
+    private float slope = -0.15f;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        float amountToMoveThisFrame = speed * Time.deltaTime;
+        Vector2 currentPosition = transform.position;
+        Vector2 newPosition = new Vector2(currentPosition.x + amountToMoveThisFrame, currentPosition.y + amountToMoveThisFrame * slope);
+        this.transform.position = newPosition;
+    }
+}
