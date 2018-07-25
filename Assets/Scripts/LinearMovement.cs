baf856096829feedc193dcdf8260a115afa53364
Joshua Skinner <jkyuusai@gmail.com>
2018-07-24 21:19:09 -0700

    Add deer and lineraMovement script

---
+++ Assets/Scripts/LinearMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearMovement : MonoBehaviour {
+    [SerializeField, Range(0, 15)]
+    private float speed;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        float amountToMoveThisFrame = speed * Time.deltaTime;
+        Vector2 currentPosition = transform.position;
+        Vector2 newPosition = new Vector2(currentPosition.x + amountToMoveThisFrame, currentPosition.y);
+        this.transform.position = newPosition;
+    }
+}
