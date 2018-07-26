4bfe405e253edb1bafe7f59bfa4443fa44a510db
Joshua Skinner <jkyuusai@gmail.com>
2018-07-25 22:34:04 -0700

    Add basic enemy generator

---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour {
+
+    public GameObject deer;
+    public GameObject bird;
+    public GameObject head;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("LoadDeer", 0f, 2f);
+        InvokeRepeating("LoadBird", 2f, 4f);
+        InvokeRepeating("LoadHead", 4f, 5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    void LoadDeer() {
+        Instantiate(deer, new Vector3(-4, 0, 0), Quaternion.identity);
+    }
+
+    void LoadBird() {
+        Instantiate(bird, new Vector3(-4, 2, 0), Quaternion.identity);
+    }
+
+    void LoadHead() {
+        Instantiate(head, new Vector3(-4, -2, 0), Quaternion.identity);
+    }
+}
