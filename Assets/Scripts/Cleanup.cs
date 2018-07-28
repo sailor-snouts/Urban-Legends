ace3a6df1422699de7eb17be2a99ea8fc1bbcefe
Joshua Skinner <jkyuusai@gmail.com>
2018-07-27 23:05:45 -0700

    Add tree and tree generator
    Add cleanup script that uses OnBecameInvisible
    to destroy gameObjects once they're no longer visible
    by the camera. Attached this to all the enemy and tree prefabs.
    (Replaces the suicide script)

---
+++ Assets/Scripts/Cleanup.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cleanup : MonoBehaviour {
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
