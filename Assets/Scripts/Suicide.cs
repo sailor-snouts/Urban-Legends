b511046ad4dd26baf47351f0491ae0e20f5c304a
Robert Parker <rparker@kodama.design>
2018-07-27 19:54:40 -0700

    enemies die after getting to far

---
+++ Assets/Scripts/Suicide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suicide : MonoBehaviour {
+    [SerializeField, Range(5, 50)]
+    private float dieAtDistance = 20f;
+
+	void Update () {
+        if (Mathf.Abs(this.transform.position.x) > this.dieAtDistance)
+        {
+            DestroyObject(this.gameObject);
+        }
+	}
+}
